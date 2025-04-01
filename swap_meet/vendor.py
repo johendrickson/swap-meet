c6d3451072670441e447ed653fbb73b2159e1c10
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-03-31 18:31:01 -0700

    Adding in Jamie's version of wave1.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,20 @@
+# JAMIE CODE
+# -------------
+class Vendor:
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+
+# NATASHA CODE
+# -------------
 class Vendor:
     pass
-
-
-#test to see if git pull works :D
