2b26bba6554ca3b465518ae2d9d38e53b1efd867
Natasha Gaye <natashagaye11@gmail.com>
2025-04-02 09:36:57 -0500

    natasha's code and passed tests for wave 1 and 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,23 @@
 class Vendor:
-    pass
+    def __init__(self,inventory=None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
 
 
-#test to see if git pull works :D
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+    
+    def get_by_id(self, item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+        return None
